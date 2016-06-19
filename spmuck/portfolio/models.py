73afa6a2d9bb7d73837b9f562cdd8bfe8b70e20e
Sean Muck <spmuck@gmail.com>
2016-06-19 12:49:26 -0500

    add portfolio models and being front end design

--- spmuck/portfolio/models.py
+++ spmuck/portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from blog.models import BlogPage
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.models import Page
 
-# Create your models here.
+
+class PortfolioPage(BlogPage):
+    project_date = models.DateField("Project Date")
+    
+    content_panels = BlogPage.content_panels + [
+         FieldPanel('project_date'),
+    ]
+
+class PortfolioIndex(Page):
+    pass
