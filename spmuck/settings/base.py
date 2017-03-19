bed6e3a358fba4d86c6c5d54de617d704a4db0e4
Sean Muck <spmcuk@gmail.com>
2017-03-19 02:32:39 -0500

    adding allowed hosts

--- spmuck/settings/base.py
+++ spmuck/settings/base.py
@@ -141,3 +141,5 @@ WAGTAIL_SITE_NAME = "spmuck"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+ALLOWED_HOSTS = ['198.58.100.173', 'spmuck.com', '127.0.0.1']
